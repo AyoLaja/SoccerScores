fd582fcde219dca0d957c674f4fe15e899b78801
Ayomide Onalaja <tuf22861@temple.edu>
2017-11-08 23:25:05 -0500

    Working on API

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: "none",
-      venueName: "none",
-      founded: "none",
-      website: "none",
-      nickname: "none"
+      name: 'none',
+      venueName: 'none',
+      founded: 'none',
+      website: 'none',
+      nickname: 'none'
     }
   }
 
-  render() {
-    const apiKey = "b030f32c6a69418ca8fd984a5040d8b5";
-    const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?&q={apiKey}`;
+  ComponentDidMount() {
+    //const apiKey = 'b030f32c6a69418ca8fd984a5040d8b5';
+    //const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL/${apiKey}`;
+    const url = 'https://api.darksky.net/forecast/ad38988aa88b4261594ade3c95519e5a/37.8267,-122.4233';
 
-    fetch(url)
-    .then(result => result.json())
-    .then(data => this.setState({
-      name: data.Teams[0].Name
-    }))
-
-    this.setState({
-      founded: "Ayo"
+    fetch(url, {
+      method: "GET",
+      body: JSON.stringify(),
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
+    .then(result => result.json())
+    .then(result => this.setState({
+      name: result
+    }))
+  }
 
+  render() {
     return (
       <div>
-        <p>{this.state.name}</p>
-
+        <p></p>
       </div>
     );
   }
