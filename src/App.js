2b429403ee526940ea8c35320399ec19d9ab3631
Ayomide Onalaja <ayo.onalaja@gmail.com>
2017-11-10 17:19:00 -0500

    Fetch API successful

--- src/App.js
+++ src/App.js
@@ -17,19 +17,7 @@ class App extends Component {
     const apiKey = '00be38b05fb4464c81e2a008d4b4e792';
     //const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key={apiKey}`;
     const url = 'https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=' + apiKey;
-    //const url = 'https://api.darksky.net/forecast/ad38988aa88b4261594ade3c95519e5a/37.8267,-122.4233';
 
-    console.log(apiKey)
-    this.setState({
-      website: 'ayo.com'
-    })
-    /*fetch(url, {
-      method: "GET",
-      body: JSON.stringify(),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })*/
     fetch(url)
     .then(response => response.json())
     .then(data => this.setState({
@@ -37,9 +25,18 @@ class App extends Component {
     }))
   }
 
+  getData() {
+    fetch('https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=00be38b05fb4464c81e2a008d4b4e792')
+    .then(response => response.json())
+    .then(data => this.setState({
+      founded: data.Teams[0].Phone
+    }))
+  }
+
   render() {
     return (
       <div>
+      <p>{this.getData()}</p>
         <p>{this.state.name}, {this.state.website}</p>
       </div>
     );
