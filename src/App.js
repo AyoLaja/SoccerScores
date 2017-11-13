6d06a34367532e832cc5a83a7c151edd95d061a1
Ayomide Onalaja <ayo.onalaja@gmail.com>
2017-11-13 17:06:48 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -10,20 +10,34 @@ class App extends Component {
       founded: 'none',
       website: 'none',
       nickname: 'none',
-      people: []
+      people: [],
+      strings: []
     }
   }
 
   getData() {
     //fetch('https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=00be38b05fb4464c81e2a008d4b4e792')
-    fetch('https://randomuser.me/api/?results=20')
-    .then(response => response.json())
-    .then(data => this.setState({
-      people: data.results
-    }))
+    const promise = fetch('https://randomuser.me/api/?results=20')
+      .then(response => {
+        if (response.status >= 400) {
+        throw `Response Invalid ( ${response.status} )`;
+        return;
+      }
+      return response.json();
+    })
+    .then(({results}) => {
+      this.setState({
+        people: results
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    return promise;
   }
 
-  ComponentDidMount() {
+  ComponenWillMount() {
     /*const apiKey = '00be38b05fb4464c81e2a008d4b4e792';
     //const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key={apiKey}`;
     const url = 'https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=' + apiKey;
@@ -37,19 +51,18 @@ class App extends Component {
     this.getData();
   }
 
-
-
   render() {
     this.getData();
 
     return (
       <div>
         <ul>
-          {this.state.people.map((item) => {
+          {this.state.people.map((person) => {
             return (
-              <li>{item[0].name}</li>
-            )
-          })}
+              <li>{person.name}</li>
+              <li>{person.email}</li>
+            });
+          )
         </ul>
       </div>
     );
