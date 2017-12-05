0dd57a02b7faa3f63b44e177e2de4af476a66cca
Ayomide Onalaja <tuf22861@temple.edu>
2017-12-04 23:25:31 -0500

    API call works

--- src/App.js
+++ src/App.js
@@ -5,55 +5,59 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      people: []
+      requestFailed: false
     }
 
     this.getData = this.getData.bind(this);
+    this.displayData = this.displayData.bind(this);
   }
 
   getData() {
     //fetch('https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=00be38b05fb4464c81e2a008d4b4e792')
-    const promise = fetch('https://randomuser.me/api/?results=20')
-      .then(response => {
-        if (response.status >= 400) {
-        throw `Response Invalid ( ${response.status} )`;
-        return;
-      }
-      return response.json();
-    })
-    .then(({results}) => {
-      return results;
-    })
-    .catch(error => {
-      console.log(error);
-    });
 
-    return promise;
+    fetch('https://randomuser.me/api/?results=20')
+    .then(response => {
+      if(!response.ok) {
+        throw Error("Network Request Failed")
+      }
+      return response
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({
+        info: data.info,
+        people: data.results
+      })
+    }, () => {
+      this.setState({
+        requestFailed: true
+      })
+    })
   }
 
-  ComponenDidMount() {
+  componentDidMount() {
     /*const apiKey = '00be38b05fb4464c81e2a008d4b4e792';
     //const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key={apiKey}`;
-    const url = 'https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=' + apiKey;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => this.setState({
-      name: 'ayo'
-    }))*/
+    const url = 'https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=' + apiKey;*/
 
     this.getData()
-      .then(data => {
-        this.setState({
-          people: data
-        });
-      });
+    this.displayData()
+  }
+
+  displayData() {
+    let randomUsers = this.state.people.email;
+    let userEmails = randomUsers.map((randomuser) =>
+      <li>{randomuser}</li>);
   }
 
   render() {
+    if(this.state.requestFailed)return <p>Failed</p>
+    if(!this.state.people) return <p>Loading...</p>
     return (
       <div>
-        <p>{this.state.people.results[0].gender}</p>
+        <p>{this.state.people[0].email}</p>
+        <ul>{displayData()}</ul>
       </div>
     );
   }
