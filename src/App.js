ecb3ce7ed099a131c9de5b15e09cb4f5bcde7b83
Ayomide Onalaja <ayo.onalaja@gmail.com>
2018-03-07 12:49:48 -0500

    Added json of players, mapped to list

--- src/App.js
+++ src/App.js
@@ -1,60 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import players from './arsenalPlayer.json';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      requestFailed: false
-    }
-
-    this.getData = this.getData.bind(this);
-  }
+  // getData() {
+  //   //fetch('https://api.fantasydata.net/v3/soccer/stats/json/PlayersByTeam/509?subscription-key=00be38b05fb4464c81e2a008d4b4e792')
+  //
+  //}
 
   getData() {
-    //fetch('https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=00be38b05fb4464c81e2a008d4b4e792')
-
-    fetch('https://randomuser.me/api/?results=20')
-    .then(response => {
-      if(!response.ok) {
-        throw Error("Network Request Failed")
-      }
-      return response
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      this.setState({
-        info: data.info,
-        people: data.results
-      })
-    }, () => {
-      this.setState({
-        requestFailed: true
-      })
-    })
-  }
-
-  componentDidMount() {
-    /*const apiKey = '00be38b05fb4464c81e2a008d4b4e792';
-    //const url = `https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key={apiKey}`;
-    const url = 'https://api.fantasydata.net/v3/soccer/scores/json/CompetitionDetails/EPL?subscription-key=' + apiKey;*/
-
-    this.getData()
+    console.log(players);
   }
 
   render() {
-    if(this.state.requestFailed)return <p>Failed</p>
-    if(!this.state.people) return <p>Loading...</p>
-    return (
-      <div>
-        <ul>{this.state.people.map((user) =>
-              <li key={user.email}>{user.login.username} - {user.name.first} {user.name.last}</li>
-            )}
+    return(
+      <div className="container">
+        <h1>Arsenal Players</h1>
+        {this.getData()}
+        <ul className="player-list">
+          {
+            players.map((players) => {
+              return (
+                <li className="list-element">
+                {players.CommonName} <br/>
+                <span className="img-container"><img src={players.PhotoUrl}/></span><br/>
+                <span>{players.Nationality}</span><br/>
+                <span>{players.Jersey}</span><br/>
+                <span>{players.Position}</span><br/>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
-    );
-  }
+    )};
 }
 
 export default App;
